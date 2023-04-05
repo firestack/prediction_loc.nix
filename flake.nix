c691244dc153fb9421862b6ed0b9507c9b3d3f7b
Kayla Firestack <kfirestack@mbta.com>
2023-04-05 08:41:31 -0400

    feat(flake): add poetry project to flake

--- flake.nix
+++ flake.nix
@@ -3,14 +3,31 @@
 
 	inputs.flake-utils.url = "github:numtide/flake-utils";
 	inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+	inputs.poetry2nix = {
+		url = "github:nix-community/poetry2nix";
+		inputs.nixpkgs.follows = "nixpkgs";
+		inputs.flake-utils.follows = "/flake-utils";
+	};
 
-	outputs = { self, nixpkgs, flake-utils }:
+	outputs = { self, nixpkgs, flake-utils, poetry2nix }:
 		flake-utils.lib.eachDefaultSystem (system:
 			let
+				# see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+				inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
 				pkgs = nixpkgs.legacyPackages.${system};
 			in
 			{
+				packages = {
+					prediction_loc = mkPoetryApplication {
+						projectDir = self;
+					};
+					default = self.packages.${system}.prediction_loc;
+				};
+
 				devShells = rec {
+					poetry = pkgs.mkShell {
+						packages = [ poetry2nix.packages.${system}.poetry ];
+					};
 					default = pipenv;
 					pipenv = let ppkgs = pkgs.python3Packages; in pkgs.mkShell {
 						packages = [
@@ -19,6 +36,8 @@
 							pkgs.pipenv
 						];
 					};
+
+
 				};
 			});
 }
